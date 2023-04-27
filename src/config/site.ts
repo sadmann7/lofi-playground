c1ba65446aba1f73865d69fe6ce4410bdd0d4e46
sadmann7 <sadmann898@gmail.com>
2023-04-28 02:43:23 +0600

    setup project, and install dependecies

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Lofi Playground",
+  description:
+    "A collection of lofi music, and images to help you focus and relax.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/sadmann17",
+    github: "https://github.com/sadmann7/lofi-playground",
+  },
+}
